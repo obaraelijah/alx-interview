72c20c588b4f3584c5da5b34eac5c49d4a710655
elijah obara <elijahobara357@gmail.com>
2023-07-09 17:25:38 +0300

    Practice quiz 2

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Method that determines if all the boxes can be opened.
+Given n number of locked boxes, each box numbered sequentially
+from 0 to n - 1, and each box may contain keys to the other boxes.
+"""
+
+def canUnlockedAll(boxes):
+    if not boxes or type(boxes) is not list:
+        return False
+    
+    unlocked = [0] 
+    
+    for x in unlocked:
+        for y in boxes[x]:
+            if y not in unlocked and y < len(boxes):
+                unlocked.append(y)
+    
+    # Check if all boxes are unlocked
+    if len(unlocked) == len(boxes):
+        return True
+    return False
