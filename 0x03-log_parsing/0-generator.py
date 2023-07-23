a8637975f14f586399e11d52fc71c24ba418aaae
obaraelijah <elijahobara357@gmail.com>
2023-07-23 14:14:52 +0300

    log parsing solution

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,45 +1,15 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics:m"""
+import random
 import sys
+from time import sleep
+import datetime
 
-
-def print_status_codes(status_codes):
-    """Prints the status code with its count.
-    Format:
-        <status>: <count>
-    """
-    for key, val in sorted(status_codes.items()):
-        print("{}: {}".format(key, val))
-
-
-code = {}
-total = 0
-vals_total = 0
-
-try:
-    for line in sys.stdin:
-        line_arr = line.split(" ")
-
-        if len(line_arr) > 4:
-            file_size = int(line_arr[-1])
-            status = line_arr[-2]
-
-            if status not in code:
-                code[status] = 1
-            else:
-                code[status] += 1
-
-            total += file_size
-            vals_total += 1
-
-        if vals_total == 10:
-            vals_total = 0
-            print("File size: {}".format(total))
-            print_status_codes(code)
-
-except Exception:
-    pass
-
-finally:
-    print("File size: {}".format(total))
-    print_status_codes(code)
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
