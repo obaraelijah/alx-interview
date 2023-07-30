7a07e22ca74dff7cbb0eb6626d6cab50b9cc37bf
obaraelijah <elijahobara357@gmail.com>
2023-07-30 18:29:01 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,7 @@ def validUTF8(data):
     Args:
             data (list[int]): a list of integers
     """
-    expected_continuation_bytes = 0
+    expected_bytes = 0
 
     # Define bit patterns for UTF-8 encoding
     UTF8_BIT_1 = 1 << 7  # 10000000
@@ -26,23 +26,23 @@ def validUTF8(data):
 
         # If we are not currently expecting any
         # continuation bytes
-        if expected_continuation_bytes == 0:
+        if expected_bytes == 0:
             # Count the number of leading 1's in the
             # current byte to determine the number of
             # continuation bytes
             while leading_one_mask & byte:
-                expected_continuation_bytes += 1
+                expected_bytes += 1
                 leading_one_mask = leading_one_mask >> 1
 
             # If the byte is not a multi-byte sequence,
             # move to the next byte
-            if expected_continuation_bytes == 0:
+            if expected_bytes == 0:
                 continue
 
             # If the number of continuation bytes is not
             # between 2 and 4, the sequence is invalid
-            if expected_continuation_bytes == 1 or\
-                    expected_continuation_bytes > 4:
+            if expected_bytes == 1 or\
+                    expected_bytes > 4:
                 return False
 
         # If we are expecting continuation bytes
@@ -53,11 +53,11 @@ def validUTF8(data):
                 return False
 
         # Decrement the expected number of continuation bytes
-        expected_continuation_bytes -= 1
+        expected_bytes -= 1
 
     # If we have processed all bytes and are not expecting
     # any more continuation bytes, the sequence is valid
-    if expected_continuation_bytes == 0:
+    if expected_bytes == 0:
         return True
     else:
         return False
