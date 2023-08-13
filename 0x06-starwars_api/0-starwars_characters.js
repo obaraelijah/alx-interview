9a3a17d8ae25ee0964d6555df30a5cbed8a3599a
obaraelijah <elijahobara357@gmail.com>
2023-08-13 22:53:26 +0300

    hot fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-import request from 'request';
+const request = require('request');
 
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
