fac82cddf133428fd22e73dacb3c5fb061d361ab
obaraelijah <elijahobara357@gmail.com>
2023-08-17 21:24:19 +0300

    Starwars api fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,30 +2,41 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
-    return;
-  }
-
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
-    }
-  });
-}
-
-request(movieEndpoint, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const characterList = JSON.parse(body).characters;
+    console.error(error);
+  }
+  if (response.statusCode === 200) {
+    (async function () {
+      const characters = JSON.parse(body).characters;
 
-    sendRequest(characterList, 0);
+      try {
+        const responses = await Promise.all(
+          characters.map((character) => requestCharacter(character))
+        );
+
+        for (const response of responses) {
+          console.log(response.name);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   }
 });
+
+function requestCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      if (response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
